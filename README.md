f235f0bd9dc65a3af8a78b84c748cfb6327a3841
Francis Robichaud <frobichaud@petalmd.com>
2016-06-06 21:11:12 -0400

    Add codeclimate

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+[![Code Climate](https://codeclimate.com/github/frobichaud/docker-github-automerge/badges/gpa.svg)](https://codeclimate.com/github/frobichaud/docker-github-automerge)
 [![CircleCI](https://circleci.com/gh/frobichaud/docker-github-automerge.svg?style=svg)](https://circleci.com/gh/frobichaud/docker-github-automerge)
 
 # GitHub AutoMerge
--- spec/webhooks/status_spec.rb
+++ spec/webhooks/status_spec.rb
@@ -3,11 +3,15 @@ require 'webhooks/status'
 
 describe Webhooks::Status do
 
+  let!(:payload) {
+    File.new('spec/fixtures/status_webhook.txt').readlines
+  }
+
   context 'when we receive a status webhook' do
 
     it 'should parse the associated repo' do
       s = Webhooks::Status.new '{}'
-      s.test
+      puts payload
       # Webhooks::Status.new '{asdf}'
     end
   end
