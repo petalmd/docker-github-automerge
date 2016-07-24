787a6d5e37c3c0d7f7670c952380549a61bc84a5
Julien Maitrehenry <julien.maitrehenry@me.com>
2016-07-23 23:20:27 -0400

    Add webhook

--- lib/merge_report.rb
+++ lib/merge_report.rb
@@ -1,5 +1,6 @@
 require 'client/pull_request'
 require 'hook_actions'
+require 'net/http'
 
 class MergeReport < HookActions
 
@@ -13,10 +14,11 @@ class MergeReport < HookActions
     pull_request = @pr_api.get @data['pull_request']['number']
 
     if action == 'closed' && !pull_request['merged_at'].nil?
-      from = pull_request['head']['label']
-      to = pull_request['base']['label']
+      from = pull_request['head']['ref']
+      to = pull_request['base']['ref']
       url = pull_request['html_url']
       report pull_request['title'], from, to, url
+      notify_selfpro @data['repository']['name'], from
     else
       @logger.info "Skipping reporting for PR ##{pull_request['number']} in #{@repo_name} - pull request still open" if @logger
     end
@@ -29,5 +31,16 @@ class MergeReport < HookActions
 
     @logger.info "Reporting status for merged PR \n#{merge_msg}" if @logger
     notify text, merge_msg, color
+  end
+
+  def notify_selfpro(repo, from)
+    @logger.info "Reporting status to SelfPro" if @logger
+
+    uri = URI.parse('http://selfpro.petalmd.com/hooks/branch:merge')
+    req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
+    data = { 'project' => repo, 'branch' => from}
+    req.body = data.to_json
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.request(req)
   end
 end
